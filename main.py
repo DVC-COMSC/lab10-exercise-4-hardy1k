01b64fdc32a98a4b300011fd6bd334dc133cc917
Jalil Mackey <126622214+hardy1k@users.noreply.github.com>
2023-03-12 04:46:47 +0000

    complete e4

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    celcius =int(input('Enter the Celcius Degrees: '))
+    fahrenheit = (9,0 / 5.0) * celcius + 32
+    print(f'The Fahrenheit is{fahrenheit: 2f}')
+
+  
     pass
 
 
